1ce5a0cbcbffb968be57126b9d4dcdec2ee59062
512seanjones <sjones7@stevens.edu>
2019-03-09 17:37:28 -0500

    Add files via upload

--- Alpha-BAHT-master/server.py
+++ Alpha-BAHT-master/server.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from form import LoginForm
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-try:
-    @app.route("/")
-    def home():
-        return render_template("home.html")
+@app.route("/")
+def home():
+    user = {'username': 'Sean'}
+    return render_template('index.html', title='Home', user=user)
 
-    @app.route("/about")
-    def about():
-        return render_template("about.html")
-    if __name__ == "__main__":
-        app.run(debug=True)
+@app.route('/index')
+def index():
+    user = {'username': 'Sean'}
+    return render_template('index.html', title='Home', user=user)
 
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
