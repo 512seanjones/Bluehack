a9b140a623a709968d68f6427501d28933fbd63e
Sean Jones <512seanjones@gmail.com>
2019-03-09 18:35:49 -0500

    taking data from form

--- Alpha-BAHT-master/form.py
+++ Alpha-BAHT-master/form.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+class DataForm(FlaskForm):
+    city = StringField('City', validators=[DataRequired()])
+    state = StringField('Street', validators=[DataRequired()])
+    street = StringField('State', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- Alpha-BAHT-master/server.py
+++ Alpha-BAHT-master/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
-from form import LoginForm
+from form import DataForm
 from config import Config
 
 app = Flask(__name__)
@@ -15,14 +15,15 @@ def index():
     user = {'username': 'Sean'}
     return render_template('index.html', title='Home', user=user)
 
-@app.route('/login')
-def login():
-    form = LoginForm()
+@app.route('/getdata')
+def getdata():
+    form = DataForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        print("City is: " + form.city)
+        print("State is: " + form.state)
+        print("Street is: " + form.street)
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('getdata.html', title='Form', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
