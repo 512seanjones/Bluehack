af6ceadd8b5454ef85fe32c07a04324ea00ba217
Sean Jones <512seanjones@gmail.com>
2019-03-09 20:07:55 -0500

    Combine server with heatmap

--- Alpha-BAHT-master/server.py
+++ Alpha-BAHT-master/server.py
@@ -1,29 +1,30 @@
 from flask import Flask, render_template, redirect, url_for
 from form import DataForm
 from config import Config
+import gmplottest
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    user = {'username': 'Sean'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
 
 @app.route('/index')
 def index():
-    user = {'username': 'Sean'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
 
-@app.route('/getdata')
+@app.route('/getdata',  methods=['GET', 'POST'])
 def getdata():
     form = DataForm()
     if form.validate_on_submit():
-        print("City is: " + form.city)
-        print("State is: " + form.state)
-        print("Street is: " + form.street)
+        gmplottest.main(str(form.street), str(form.city), str(form.state))
         return redirect(url_for('index'))
     return render_template('getdata.html', title='Form', form=form)
+
+@app.route('/my_map')
+def my_map():
+    return render_template('my_map.html', title='Heatmap')
 
 if __name__ == "__main__":
     app.run(debug=True)
